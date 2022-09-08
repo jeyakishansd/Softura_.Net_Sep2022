ab195a09df9c7fd070594353721eac7974950513
Jeyakishan  S D <94844789+jeyakishansd@users.noreply.github.com>
2022-09-08 20:54:57 +0530

    Add files via upload

---
+++ Task3ArithmeticOperations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Add_Sub
+    {
+        public void add(int i,int j)
+        {
+            Console.WriteLine(i + j);
+        }
+        public void sub(int i,int j)
+        {
+            Console.WriteLine(i - j);
+        }
+    }
+    class Multiply_Divide : Add_Sub
+    {
+        public void multiply(int i,int j)
+        {
+            Console.WriteLine(i* j);
+        }
+        public void divide(int i,int j)
+        {
+            Console.WriteLine(i/j);
+        }
+    }
+    internal class Task3ArithmeticOperations
+    {
+        public static void Main()
+        {
+            Multiply_Divide mobj = new Multiply_Divide();
+            mobj.add(2,1);
+            mobj.sub(8,2);
+            mobj.multiply(3,3);
+            mobj.divide(18, 2);
+        }
+    }
+}
+
---
+++ Task3Poly.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Task3Poly
+    {
+        public  void userLogin(String email,String Password)
+        {
+            Console.WriteLine("EmailID : {0} , Password : {1}",email,Password);
+        }
+        public void userLogin(String membership_ID, int pin)
+        {
+            Console.WriteLine("Membership_ID : {0} , Pin : {1}",membership_ID,pin);
+        }
+        public void userLogin(long Mobile_Number, int pin)
+        {
+            Console.WriteLine("Mobile number : {0} ,Pin :{1}",Mobile_Number,pin);
+
+        }
+        public static void Main()
+        {
+            Task3Poly Poly = new Task3Poly();
+            Poly.userLogin("jeyaki.kishan@gmail.com", "jeykee");
+            Poly.userLogin("JK3699", 3699         );
+            Poly.userLogin(7639575709, 3699);
+            
+        }
+
+    }
+}
